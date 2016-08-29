8d32da585d904f06acdf301c71fecd8ff9e6184e
aiido <sejmen.idaro@hotmail.com>
2016-08-29 09:43:21 +0200

    Pushing new

---
+++ VCARD_Test/public/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aida PC
+ * Date: 29.08.2016
+ * Time: 09:08
+ */
+
+require __DIR__.'/../src/Bootstrap.php';
+
---
+++ VCARD_Test/src/Bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aida PC
+ * Date: 29.08.2016
+ * Time: 09:12
+ */
+
+echo 'Hello Wordl!';
